cccaaf84d0afec56874e8d80d10a7fd7886becb0
André Gomes <andregds85@gmail.com>
2021-10-27 17:15:54 -0300

    teste
    teste

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,12 @@
+<li><a class="nav-link" href="{{ url('/') }}">Inicio</a></li>
+<li><a class="nav-link" href="{{ route('pacientes.index') }}">Regulação</a></li>
+<li><a class="nav-link" href="{{ url('soudohospital') }}"> Sou do Hospital</a></li>
+<li><a class="nav-link" href="{{ url('users') }}"> Administrador</a></li>
+<li><a class="nav-link" href="{{ url('municipio') }}"> Municipio</a></li>
+<li><a class="nav-link" href="{{ url('regulador') }}"> Médico Regulador</a></li>
+
+
+
+
+
+
